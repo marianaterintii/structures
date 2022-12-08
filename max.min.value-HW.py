d9c6df2f46ee0c3856a7d99b6e039b7ad4af8b93
Mariana Terintii <117921869+marianaterintii@users.noreply.github.com>
2022-12-08 17:04:06 +0200

    Add files via upload

---
+++ max.min.value-HW.py
@@ -0,0 +1,14 @@
+from os import system
+system ("cls")
+
+# HW: find the max/minim values and print:
+
+data = [-10, 0, 5, 10, -15, 20, 0]
+
+max_n = max (data)
+print ("Maximum value:", max_n)
+
+min_n = min (data)
+print ("Minimum value:", min_n)
+
+print ()
